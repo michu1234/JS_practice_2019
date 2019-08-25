7fab5099a128aebd86542c362023517eb6e23aef
Dawid Nawrocki <dawiddeveloper@gmail.com>
2019-08-25 23:09:03 +0200

    time format changer

--- hackerrank_10.js
+++ hackerrank_10.js
@@ -1,16 +1,25 @@
-let initialTime = "12:00:00AM";
+let initialTime = "12:33:00PM";
 // change time format to 24h
 
 function changeTimeFormat(time) {
-  let regPattern = /(PM)/;
-  let isPM = time.match(regPattern);
+  let regPatternPM = /(PM)/;
+  let regPatternAM = /(AM)/;
+  let isPM = time.match(regPatternPM);
   let hour = time.slice(0, 2);
 
   if (isPM) {
     let twoDigitHour = Number(hour) + 12; /*?*/
+    if(hour === "12") {
+      return time.substring(0, time.length - 2) /*?*/
+    }
     return `${twoDigitHour}:${time.slice(3, 8)}`; /*?*/
+  } else if(regPatternAM) {
+      if(hour === '12') {
+        return `00:${time.slice(3, 8)}`; /*?*/
+      }
+    return time.substring(0, time.length - 2) /*?*/
   } else {
-    return time.substring(0, str.length - 2)
+    return time; /*?*/ 
   }
 }
 
