c6dfbfd5a94b19fcd0d9dbd683943497c71cfc26
Dawid Nawrocki <dawiddeveloper@gmail.com>
2019-08-31 04:53:31 +0200

    lazy function definition

--- lazy_function_def.js
+++ lazy_function_def.js
@@ -5,11 +5,22 @@ let work = () => {
   work = () => {
    console.log("now let's do the rest of the work");
   }
-
-  work = () => {
-    console.log('third time');
-  }
  }
 
 work();
 work();
+
+
+// generator alternative
+
+function* job() {
+  console.log("do some initial work here");
+  yield "done 1/2";
+  console.log("now let's do the rest of the work");
+  yield "done 1/1"
+}
+
+
+let j = job();
+j.next();
+j.next();
